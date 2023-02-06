5dbed666d7a4e2859382501cdefb571c17a1a39b
Kong-E <kng001016@gmail.com>
2023-02-07 07:11:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,104 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [toDo, setToDo] = useState();
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
+  const [loading, setLoading] = useState(true);
+  const [clicked, setClicked] = useState(false);
+  const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState();
+  const [number, setNumber] = useState(false);
+  const [name, setName] = useState();
+  const [asset, setAsset] = useState();
+  const [price, setPrice] = useState();
+
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(toDo);
-    setToDo("");
-    setToDos((current) => [toDo, ...current]);
+    setName(value);
+    setAsset(number);
+    coins.forEach((coin) =>
+      coin.name === value ? setPrice(coin.quotes.USD.price) : null
+    );
   };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          type="text"
-          placeholder="Write your todo ..."
-          onChange={onChange}
-        ></input>
-        <button>Add To Do</button>
-        <br />
-        <ul>
-          {toDos.map((item, index) => (
-            <li key={index}>{item}</li>
+    <div className={styles.body}>
+      <h1>🪙 Coin Calculator 🪙</h1>
+      {loading ? "" : <h2>There are {coins.length}🪙s...</h2>}
+      {loading ? (
+        <strong>로딩 중...</strong>
+      ) : (
+        <select>
+          <option> See the Coin List </option>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price.toFixed(5)}{" "}
+              USD
+            </option>
           ))}
-        </ul>
+        </select>
+      )}
+      <hr />
+
+      <form>
+        <input
+          value={value}
+          placeholder="Type the symbol"
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+        />
+        <br />
+        <input
+          value={number}
+          placeholder="Type your own money"
+          onChange={(e) => setNumber(e.target.value)}
+          type="number"
+        />
+        <br />
+        <input value="Submit" type="button" onClick={onSubmit} />
       </form>
+
+      {name !== undefined
+        ? coins.map((coin) =>
+            coin.name === name ? (
+              <div key={coin.id}>
+                <span>
+                  {coin.name} ({coin.symbol}): $
+                  {coin.quotes.USD.price.toFixed(5)} USD
+                </span>
+                <button
+                  onClick={() => setClicked(true)}
+                  style={{ marginLeft: 5 }}
+                >
+                  Calculate
+                </button>
+              </div>
+            ) : null
+          )
+        : null}
+
+      {clicked ? `You can buy ${name} by ${asset / price}.` : null}
+
+      <div>
+        <button
+          onClick={() => {
+            setClicked(false);
+            setName();
+            setValue("");
+            setNumber("");
+          }}
+        >
+          Refresh
+        </button>
+      </div>
     </div>
   );
 }
