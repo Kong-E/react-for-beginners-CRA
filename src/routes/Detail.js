5bef04fbc6630bef003389cef78471dfb0513241
Kong-E <kng001016@gmail.com>
2023-02-11 08:22:18 +0900

    Detail.js 구성

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,29 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState("");
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data);
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <img src={movie.large_cover_image} />
+      <h1>{movie.title_long}</h1>
+      <div>{movie.rating}</div>
+      {movie.genres?.map((genre) => (
+        <span key={genre}>{genre} </span>
+      ))}
+      <div>{movie.description_intro}</div>
+    </div>
+  );
 }
 export default Detail;
